5154f2a08987fe8dc796edd98d560d91266904d1
georgejeswin <jeswinmyladoor@gmail.com>
2023-02-23 01:15:27 +0530

    Added about, expertise cards

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        lightBlue: "#5BE9B9",
+      },
+    },
+  },
+  plugins: [],
+};
